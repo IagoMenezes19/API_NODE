0e3d030e4a742bf230a4469786bfead5bc57d43c
IagoMenezes19 <iagomenez1906@gmail.com>
2025-09-23 23:39:29 -0300

    API ta nos trinques agora

--- server.js
+++ server.js
@@ -1,34 +1,73 @@
 //OBJ
 
 import express from 'express'
+import pkg from "@prisma/client"
+import cors from 'cors'
 
-import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
-const prisma = new PrismaClient();
+const { PrismaClient } = pkg
+const prisma = new PrismaClient()
 
 const app = express()
+app.use(cors())
 app.use(express.json())
+
+const usuarios = []
 
 //Rota
 
 app.post('/cadastro', async (req, res) => {
-    await prisma.Usuario.create({
-        data:{
-            email:req.body.email,
+
+    await prisma.usuario.create({
+        data: {
+            email: req.body.email,
             nome: req.body.nome,
             idade: req.body.idade,
         }
     })
-    // res.send('POST certin')
+
     res.status(201).json(req.body)
 })
 
 
 app.get('/cadastro', async (req, res) => {
-    const lista_usuarios = await prisma.usuario.findMany()
+
+    const lista_usuarios = await prisma.usuario.findMany();
 
     res.status(200).json(lista_usuarios)
+
 })
+
+app.put('/cadastro/:id', async (req, res) => {
+
+    await prisma.usuario.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            nome: req.body.nome,
+            idade: req.body.idade,
+        }
+    })
+
+
+    res.status(201).json({ "message": "Usuário atualizado" })
+
+})
+
+app.delete('/cadastro/:id', async (req, res) => {
+
+    await prisma.usuario.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+
+    res.status(201).json({ "message": "Usuário deletado" })
+
+})
+
 
 //Porta
 
